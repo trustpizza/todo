e3a3d4a30ffbaa87b8818ead86500eb0977e12b1
trustpizza <maxelolsson@gmail.com>
2023-02-04 11:14:32 -0600

    NPM install css loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-   filename: 'bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
