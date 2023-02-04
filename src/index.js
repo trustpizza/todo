03ed51eeae1cc816e0119d000db62adcc694373d
trustpizza <maxelolsson@gmail.com>
2023-02-04 14:03:34 -0600

    Remove default event for modal form

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import TodoProject from "./todoProject";
 import { generateTaskModal } from './taskModal';
 
 document.body.appendChild(generateTaskModal())
+
+// Modal Submit Listener
+
+document.getElementById('modal-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+});
