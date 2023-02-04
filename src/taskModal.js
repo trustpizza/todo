51a34af53e7e62f8291b24de40b57ab597b151d4
trustpizza <maxelolsson@gmail.com>
2023-02-04 12:47:16 -0600

    Create prototype of modal form generator

--- src/taskModal.js
+++ src/taskModal.js
@@ -13,11 +13,42 @@ export function generateTaskModal() {
     modalForm.id = 'modal-form';
 
     let title = document.createElement('input');
+    title.type = 'text';
     title.placeholder = 'Title';
     title.required = true;
     title.maxLength = 20;
     title.className = 'form-item title';
 
-    
+    let description = document.createElement('input');
+    description.type = 'text';
+    description.placeholder = "Description...";
+    description.required = false;
+    description.maxLength = 240;
+    description.className = 'form-item description';
 
+    let dueDateLabel = document.createElement('label');
+    dueDateLabel.for = 'due-date';
+    let dueDate = document.createElement('input');
+    dueDate.type = 'date';
+    dueDate.name = 'due-date';
+    dueDate.className = 'form-item due-date';
+
+    let submit = document.createElement('input');
+    submit.type = 'submit';
+    submit.value = 'DONE';
+    submit.className = 'form-item submit';
+    submit.id = 'submit';
+
+    //  Add all items to modal-form
+    modalForm.appendChild(title);
+    modalForm.appendChild(description);
+    modalForm.appendChild(dueDateLabel);
+    modalForm.appendChild(dueDate);
+    modalForm.appendChild(submit);
+
+    // Append form to modal
+    modalContent.appendChild(modalForm);
+    modal.appendChild(modalContent);
+
+    return { modal };
 }
